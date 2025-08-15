aaae51d529011534a8c00a83b4634306f781bfb8
khanxmetu <khanxbahria@gmail.com>
2025-08-15 15:18:39 +0500

    Fix path

--- snapshot/backup.sh
+++ snapshot/backup.sh
@@ -12,6 +12,6 @@ expiry_timestamp=$(($(date +%s) + 24 * 60 * 60))
 echo "127.0.0.1:8000	FALSE	/	FALSE	$expiry_timestamp	auto_login	$USERNAME" > $COOKIES
 
 wget --no-host-directories -P ./django_admin_demo_snapshot --mirror --no-parent --reject-regex "(.*)\?(.*)" --load-cookies $COOKIES $HOST
-wget --no-host-directories -P ./django_admin_demo_snapshot/static/admin/js/vendor/hotkey --load-cookies $COOKIES $HOST/django-admin-keyshortcuts-demo/static/admin/js/vendor/hotkey/hotkey.js
+wget --no-host-directories -P ./django_admin_demo_snapshot/django-admin-keyshortcuts-demo/static/admin/js/vendor/hotkey --load-cookies $COOKIES $HOST/django-admin-keyshortcuts-demo/static/admin/js/vendor/hotkey/hotkey.js
 
 mv ./django_admin_demo_snapshot/index.html ./django_admin_demo_snapshot/django-admin-keyshortcuts-demo/index.html
